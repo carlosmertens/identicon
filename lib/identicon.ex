09bf66a03e81cf49cf4bdb013b5a95a83d096994
Carlos Mertens <mertenscarlos@yahoo.com>
2024-10-31 12:13:28 +0100

    feat: create functions to build grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -12,6 +12,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   @doc """
@@ -25,7 +26,32 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
+  @doc """
+  Function to pick the first 3 values of the hex list and create the RGB color for the identicon.
+  """
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  @doc """
+  Function to build grid for the identicon. Uses another helper function `mirrow_row`
+  """
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirrow_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  @doc """
+  Helper Function to mirrow the first and the second values of the row and transport them to the end inversed.
+  """
+  def mirrow_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 end
--- lib/image.ex
+++ lib/image.ex
@@ -2,5 +2,5 @@ defmodule Identicon.Image do
   @moduledoc """
   Module to handle and hold the data
   """
-  defstruct hex: nil, color: nil
+  defstruct hex: nil, color: nil, grid: nil
 end
