8dffb4b5c5fe21488238db2cb85a210883b554b6
Carlos Mertens <mertenscarlos@yahoo.com>
2024-11-01 11:54:09 +0100

    feat: create functions to draw the identicon

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,6 +1,10 @@
 defmodule Identicon do
   @moduledoc """
   Takes a name and convert it into a identicon image.
+
+  The image would be 250px x 250px. Inside the image we have 25 squares of 50px.
+
+  The Identicon should be unique to the string provided. In other words, if we provide carlos as many time as we want, the identicon should be the same but diferent for a diferent name.
   """
 
   @doc """
@@ -12,6 +16,9 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_square
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   @doc """
@@ -61,5 +68,44 @@ defmodule Identicon do
     grid = Enum.filter(grid, fn {code, _index} -> rem(code, 2) == 0 end)
 
     %Identicon.Image{image | grid: grid}
+  end
+
+  @doc """
+  Function to map the grid and get the pixel coordinates of the squares in the grid.
+  """
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  @doc """
+  Function to draw the image.
+  """
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  @doc """
+  Function to save image.
+  """
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 end
--- lib/image.ex
+++ lib/image.ex
@@ -2,5 +2,5 @@ defmodule Identicon.Image do
   @moduledoc """
   Module to handle and hold the data
   """
-  defstruct hex: nil, color: nil, grid: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
